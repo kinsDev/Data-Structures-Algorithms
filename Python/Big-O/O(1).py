7b589c3e80f9d83fd6665792f8458b47e486fb4b
kinsDev <kinsleygitonga@gmail.com>
2024-12-21 20:40:29 +0300

    O(1) operations always remain constant

---
+++ Python/Big-O/O(1).py
@@ -0,0 +1,4 @@
+# As n increases in O(1), the number of operations remains constant
+
+def item_lists(n):
+    return n + n + n # we can increase n as much as we want, but the number of operations will always be constant
