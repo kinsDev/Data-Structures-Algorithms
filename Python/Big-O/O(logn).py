0166c3194a3d3d841d345ed7d27b62fe845d3df5
kinsDev <kinsleygitonga@gmail.com>
2024-12-23 20:58:17 +0300

    O(log n)

---
+++ Python/Big-O/O(logn).py
@@ -0,0 +1,2 @@
+# To find a number in a list you need to sort it first, which is O(n log n), then you can do a binary search, 
+# which is O(log n), so the total time complexity is O(n log n + log n) = O(n log n)
