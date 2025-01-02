84b39c289568ea481ba84cad38c3fff319ecc579
kinsDev <kinsleygitonga@gmail.com>
2025-01-02 12:32:16 +0300

    Popping values in a linked list

--- Python/Linked-Lists/pop.py
+++ Python/Linked-Lists/pop.py
@@ -1,60 +1,58 @@
-# We are going to focus on writing a method that appends a new node to the end of a Linked List.
+# Create a constructor for the Node class
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-
+    
 class LinkedList:
-    # Constructor for Linked Lists
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
     
-    # Method to print the Linked List
     def printer(self):
         printer = self.head
-        while printer is not None:
+        while printer != None:
             print(printer.value)
             printer = printer.next
     
-    # Method to append a new node to the end of the Linked List
     def append(self, value):
         new_node = Node(value)
-        if self.head is None:
+        if self.head is Node:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
             self.tail = new_node
+        
         self.length += 1
         return True
     
     def pop(self):
-        if self.length == 0: # case when the LL is empty
-            return None
-        # If there's only one element in the LL
+        if self.length == 0:
+            return False
         if self.length == 1:
             popped_node = self.head
             self.head = None
             self.tail = None
         else:
-            # Traverse to the second last node
+            # we will need to go to the previous node
             current_node = self.head
             while current_node.next != self.tail:
                 current_node = current_node.next
-            
-            # update and discpnnect the last node
+
+            # update and disconnect the last node
             popped_node = self.tail
             self.tail = current_node
             self.tail.next = None
         self.length -= 1
         return popped_node.value
 
+
+
 my_linked_list = LinkedList(4)
 my_linked_list.append(5)
 my_linked_list.printer()
-print("\n")
-my_linked_list.pop()
+print("Popped value:", my_linked_list.pop())
 my_linked_list.printer()
