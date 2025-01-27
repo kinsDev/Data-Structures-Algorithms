fdae7bf112ec0522dde473ebd6fbc72f5c59de61
kinsDev <kinsleygitonga@gmail.com>
2025-01-27 13:41:07 +0300

    Assigning values from the main code

--- Python/OOP-in-Python/Classes-and-Objects/Employee.py
+++ Python/OOP-in-Python/Classes-and-Objects/Employee.py
@@ -1,3 +1,4 @@
+# Assigning values in the main code
 class Employee:
     # Properties of that class and assigned with None
     # If we don't initialize the properties in the Employee Class, the code will not run
@@ -8,8 +9,20 @@ class Employee:
     salary = None
     department = None
 
-# To access the properties of an object in a class, the dot notation in used e.g Employee.ID
+# Create an object for the Employee class
+Kinsley = Employee()
 
+# Assign values to properties of Kinsley - an object of the Employee class
+Kinsley.ID = 388700
+Kinsley.salary = 3500
+Kinsley.department = "AI & Data"
+
+print("Kinsley's ID is", Kinsley.ID)
+print("Kinsley's salary is", Kinsley.salary)
+print("Kinsley's department is", Kinsley.department)
+print("\n")
+
+# Assigning values when defining the class 
 class Employees:
     # I am defining properties of a class and assigning values to them
     id = 3789
@@ -22,6 +35,7 @@ class Employees:
 Steve = Employees()
 
 # lets print the properties of Steve - an object of an Employees class
-print("Steve's ID No. is ", Steve.id)
-print("Steve's salary is ", Steve.salarys)
-print("Steve's department is ", Steve.departments)
+# To access the properties of an object in a class, the dot notation in used e.g Employee.ID
+print("Steve's ID No. is", Steve.id)
+print("Steve's salary is", Steve.salarys)
+print("Steve's department is", Steve.departments)
