0238eed407c47af4d4decdd5ca4b16d6404a000e
kinsDev <kinsleygitonga@gmail.com>
2025-01-30 00:24:08 +0300

    Encapsulation details

---
+++ Python/OOP-in-Python/Encapsulation-InformationHiding/1-Encapsulation.py
@@ -0,0 +1,14 @@
+# Other than Abstraction, Encapsulation is one of the components of data hiding.
+
+# Encapsulation refers to binding data and the methods to manipulate that data together
+# in a single unit called, class.
+
+# A class can be thought of as a capsule, having methods, properties, and objects inside it.
+
+# When encapsulating a class , a good convention is to declare all variables in that class
+# as private to prevent access by code outside the class.
+
+# If the methods and variables are encapsulated in a class, then how can they be used outside of that class?
+# The answer to this is simple. 
+# One has to implement public methods to let the outside world communicate with this class. 
+# These methods are called getters and setters. We can also implement other custom methods.
