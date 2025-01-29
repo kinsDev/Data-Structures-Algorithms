c14bbbf16ff5bf0375b88ee96981da3d5b0038b7
kinsDev <kinsleygitonga@gmail.com>
2025-01-29 23:53:27 +0300

    calculating student's performance by using classes concept

---
+++ Python/OOP-in-Python/Classes-and-Objects/Calculate-student-performance.py
@@ -0,0 +1,22 @@
+# Challenge 2: Calculate the Student's Performance
+class Student:
+    # initializer
+    def __init__(self, name, phy, chem, bio):
+        self.name = name
+        self.phy = phy
+        self.chem = chem
+        self.bio = bio
+    
+    # methods
+    def totalObtained(self):
+        # instance variable
+        marksObtained = self.phy + self.chem + self.bio # marksObtained is a local method within the totalObtained method
+        return marksObtained
+    # to get the total marks I should call the totalObtained method
+    def percentage(self):
+        return (self.totalObtained()/300) * 100
+
+Obj1 = Student("Mark", 80, 90, 40)
+
+print("Total marks obtained:", Obj1.totalObtained())
+print("Percentage of total marks obtained:", Obj1.percentage())
