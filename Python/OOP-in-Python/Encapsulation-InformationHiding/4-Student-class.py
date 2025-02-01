cb6eb03914d3cb0bb5cd05d0b6261923d5b4d5a7
kinsDev <kinsleygitonga@gmail.com>
2025-02-01 08:32:02 +0300

    Implement the Complete Student Class

---
+++ Python/OOP-in-Python/Encapsulation-InformationHiding/4-Student-class.py
@@ -0,0 +1,42 @@
+# Implementing the Complete Student Class
+
+class Student:
+    def __init__(self, name, rollNumber):
+        self.__name = name
+        self.__rollNumber = rollNumber
+    
+    def setName(self, newSetName):
+        self.__name = newSetName
+
+    def getName(self):
+        return self.__name
+    
+    def setRollNumber(self, newRollNumber):
+        self.__rollNumber = newRollNumber
+
+    def getRollNumber(self):
+        return self.__rollNumber
+
+obj1 = Student("Kinsley", 1073)
+print(obj1.getName())
+obj1.setName("Kaimenyi")
+print(obj1.getName())
+print(obj1.getRollNumber())
+obj1.setRollNumber(1503)
+print(obj1.getRollNumber())
+
+
+# Lets solve this question without using the init method
+class Student:
+    def setName(self, name):
+        self.__name = name
+    
+    def getName(self):
+        return self.__name
+    
+    def setRollNumber(self, rollNumber):
+        self.__rollNumber = rollNumber
+    
+    def getRollNumber(self):
+        return self.__rollNumber
+    
