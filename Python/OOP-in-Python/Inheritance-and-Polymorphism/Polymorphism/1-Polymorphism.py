0a5cd3897c91116b26260b7a25f9ab79ca339ed2
kinsDev <kinsleygitonga@gmail.com>
2025-02-06 23:19:14 +0300

    Polymorphism intro

---
+++ Python/OOP-in-Python/Inheritance-and-Polymorphism/Polymorphism/1-Polymorphism.py
@@ -0,0 +1,5 @@
+# Poly meaning "many" & morph meaning "forms"
+
+# In programming, polymorphism refers to the same object exhibiting different forms and behaviors.
+# For example, take the Shape Class. The exact shape you choose can be anything. 
+# It can be a rectangle...
