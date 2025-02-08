235c26d2f54874e7c51abb5bd5f9436e4d41db4d
kinsDev <kinsleygitonga@gmail.com>
2025-02-08 23:35:30 +0300

    Introduction to Object Relations

---
+++ Python/OOP-in-Python/Object-Relationships/1-Introduction.py
@@ -0,0 +1,19 @@
+# While inheritance represents a relationship between classes, 
+# there are situations where there are relationships between objects.
+
+# There are three main class relationships we need to know:
+# IS A (Inheritance)
+# Part-of
+# Has-a
+
+
+# Part-of
+# In this relationship, one class object is a component of another class object. 
+# Given two classes, class A and class B, they are in a part-of relation if a class A object
+# is a part of class B object, or vice-versa.
+
+
+# Has-a
+# This is a slightly less concrete relationship between two classes. 
+# Class A and class B have a has-a relationship if one or both need the 
+# other’s object to perform an operation, but both class objects can exist independently of each other.
