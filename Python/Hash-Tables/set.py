fc21b24a303c032d3043a708b3a9ad917e1d3553
kinsDev <kinsleygitonga@gmail.com>
2025-05-13 16:11:25 +0300

    Getting the value of a certain key from a hash table

--- Python/Hash-Tables/set.py
+++ Python/Hash-Tables/set.py
@@ -14,6 +14,15 @@ class HashTable:
     def print_table(self):
         for i, val in enumerate(self.data_map):
             print(i, ":", val)
+    
+    def set_value(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
 
 my_hash = HashTable()
+my_hash.set_value("bolts", 1400)
+my_hash.set_value("washers", 50)
+my_hash.set_value("lumber", 70)
 my_hash.print_table()
